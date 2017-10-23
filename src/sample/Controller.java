45b07203d599722dd237a6877378b840fa439270
Jigme Lobsang lepcha <jigme16045@iiitd.ac.in>
2017-10-23 21:38:22 +0530

    GUI done

--- src/sample/Controller.java
+++ src/sample/Controller.java
@@ -1,4 +1,41 @@
 package sample;
 
-public class Controller {
+
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+
+import javafx.scene.Scene;
+
+import javafx.scene.control.Button;
+import javafx.scene.layout.AnchorPane;
+import javafx.scene.layout.VBox;
+import javafx.stage.Stage;
+
+import java.io.IOException;
+
+public class Controller
+{
+    @FXML
+    Button signUpButton;
+
+
+
+    public void thug() throws IOException
+    {
+        System.out.println("thug");
+        Stage curStage=(Stage) signUpButton.getScene().getWindow();
+
+
+            try
+            {
+                Main.root=FXMLLoader.load(getClass().getResource("SignUp.fxml"));
+                curStage.setScene(new Scene(Main.root, 1000, 1000));
+                curStage.setTitle("Sign Up hoe");
+                curStage.show();
+            }
+            catch (IOException e1)
+            {
+                e1.printStackTrace();
+            }
+    }
 }
--- src/sample/Main.java
+++ src/sample/Main.java
@@ -6,18 +6,28 @@ import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
 
-public class Main extends Application {
+
+public class Main extends Application
+{
+
+    public static Parent root;
+
+
 
     @Override
-    public void start(Stage primaryStage) throws Exception{
-        Parent root = FXMLLoader.load(getClass().getResource("sample.fxml"));
-        primaryStage.setTitle("Hello World");
-        primaryStage.setScene(new Scene(root, 300, 275));
+    public void start(Stage primaryStage) throws Exception
+    {
+        //System.out.println(new File(""));
+        root = FXMLLoader.load(getClass().getResource("Login.fxml"));
+        primaryStage.setTitle("Login");
+        primaryStage.setScene(new Scene(root, 1000, 1000));
         primaryStage.show();
+
+
     }
 
-
-    public static void main(String[] args) {
+    public static void main(String[] args)
+    {
         launch(args);
     }
 }
