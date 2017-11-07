57b77caaa8b64a0bffa83be4fa8dd8abf4f529f4
Jigme Lobsang lepcha <jigme16045@iiitd.ac.in>
2017-11-07 11:38:51 +0530

    New Stuff

--- src/Classes/Room.java
+++ src/Classes/Room.java
@@ -4,4 +4,34 @@ public class Room {
     private String roomNumber;
     private int roomCapacity;
     private boolean roomAvailability;
+
+    public String getRoomNumber()
+    {
+        return roomNumber;
+    }
+
+    public int getRoomCapacity()
+    {
+        return roomCapacity;
+    }
+
+    public boolean getRoomAvailability()
+    {
+        return roomAvailability;
+    }
+
+    public void setRoomNumber(String rn)
+    {
+        roomNumber = rn;
+    }
+
+    public void setRoomCapacity(int rc)
+    {
+        roomCapacity = rc;
+    }
+
+    public void setRoomAvailability(boolean ra)
+    {
+        roomAvailability = ra;
+    }
 }
